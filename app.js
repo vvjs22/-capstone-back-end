90830d1ff1268166f927e299ec137e1e4142728f
Bo Latt <bolattny@gmail.com>
2023-04-30 10:18:08 -0400

    add boiler plate

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the app");
+});
+
+module.exports = app;

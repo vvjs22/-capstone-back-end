8e54a88e60b41fa1dadb9fcb4b675cfe39bfbc50
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2023-05-09 04:37:52 -0400

    DB added

---
+++ happndb/schema.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS happndb;
+CREATE database happndb;
+
+-- Create the User table
+CREATE TABLE "User" (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  f_name VARCHAR(255) NOT NULL,
+  l_name VARCHAR(255) NOT NULL,
+  interests VARCHAR(255),
+  twitch_channel TEXT
+);
+
+-- Create the Event table
+CREATE TABLE "Event" (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  date DATE NOT NULL,
+  time TIME NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL CHECK (LOWER(city) IN ('brooklyn', 'queens', 'manhattan', 'staten island', 'bronx', 'new york')) ,
+  state VARCHAR(255) NOT NULL CHECK (LOWER(state) = 'ny'),
+  zip INTEGER NOT NULL,
+  img_link TEXT,
+  organizer_user_id INTEGER NOT NULL,
+  checked_in_users INTEGER,
+  FOREIGN KEY (organizer_user_id) REFERENCES "User" (id)
+);
+
+-- Create the Live_video table
+CREATE TABLE "Live_video" (
+  id SERIAL PRIMARY KEY,
+  video_id VARCHAR(255) NOT NULL,
+  user_id INTEGER NOT NULL,
+  event_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "User" (id),
+  FOREIGN KEY (event_id) REFERENCES "Event" (id)
+);
+
+-- Create the Event_attendee table with a primary key
+CREATE TABLE "Event_attendee" (
+  user_id INTEGER NOT NULL,
+  event_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "User" (id),
+  FOREIGN KEY (event_id) REFERENCES "Event" (id),
+  PRIMARY KEY (user_id, event_id)
+);
